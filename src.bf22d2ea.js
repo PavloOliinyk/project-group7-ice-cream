0748ed5fd99af89211dc1511b3b78df2d98f790a
PavloOliinyk <oliinyk.pavlo@gmail.com>
2021-04-21 21:11:24 +0000

    Deploying to gh-pages from @ PavloOliinyk/project-group7-ice-cream@0942000131578e9b41f494c6c2c4376f94fba568 🚀

--- src.bf22d2ea.js
+++ src.bf22d2ea.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-group7-ice-cream/src.7f21a548.js.map
+//# sourceMappingURL=/project-group7-ice-cream/src.bf22d2ea.js.map
